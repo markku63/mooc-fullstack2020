b7f389ff524d76f0ce03a6fb6ebd0a06c16c0e48
Markku Kolkka <markku.kolkka@iki.fi>
2020-11-30 16:53:36 +0200

    Tehtävä 2.15

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -28,9 +28,14 @@ const App = () => {
     if (persons.findIndex(element => element.name === newName) !== -1) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(personObject))
+          setNewName('')
+          setNewNumber('')
+        })
+      
     }
   }
 
