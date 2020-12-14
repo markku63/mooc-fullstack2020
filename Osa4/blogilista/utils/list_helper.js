aed36752d0b3d7e95b35b705f4a603b00d024063
Markku Kolkka <markku.kolkka@iki.fi>
2020-12-14 11:25:45 +0200

    Tehtävä 4.4

--- Osa4/blogilista/utils/list_helper.js
+++ Osa4/blogilista/utils/list_helper.js
@@ -3,6 +3,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return 0
+  } else {
+    return blogs
+      .map(blog => blog.likes)
+      .reduce((acc, cur) => acc + cur)
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
