70a7cbfe0ed28f82b1c3bb9e56b098ca33447e6f
Markku Kolkka <markku.kolkka@iki.fi>
2021-01-04 10:25:23 +0200

    Tehtävä 4.21

--- Osa4/blogilista/controllers/blogs.js
+++ Osa4/blogilista/controllers/blogs.js
@@ -35,7 +35,19 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if(!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const userid = decodedToken.id
+  const blog = await Blog.findById(request.params.id)
+  if ( !blog ) {
+    return response.status(404).json({ error: 'no such blog' })
+  }
+  if ( blog.user.toString() !== userid.toString()) {
+    return response.status(403).json({ error: 'invalid user' })
+  }
+  await blog.delete()
   response.status(204).end()
 })
 
