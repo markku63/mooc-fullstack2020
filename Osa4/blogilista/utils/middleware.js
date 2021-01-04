5e9d9023c5018ceb66043dfd84bf06d95baa3ed0
Markku Kolkka <markku.kolkka@iki.fi>
2021-01-04 09:48:14 +0200

    Tehtävä 4.20

--- Osa4/blogilista/utils/middleware.js
+++ Osa4/blogilista/utils/middleware.js
@@ -23,8 +23,17 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+  next()
+}
+
 module.exports = {
   errorHandler,
   unknownEndpoint,
-  requestLogger
+  requestLogger,
+  tokenExtractor
 }
