c3f9b5e4ff0728a3c8ffecac7731898bc19589b4
Markku Kolkka <markku.kolkka@iki.fi>
2021-01-10 12:47:06 +0200

    Tehtävä 5.7

--- Osa5/blogilista-frontend/src/components/Blog.js
+++ Osa5/blogilista-frontend/src/components/Blog.js
@@ -1,8 +1,30 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const handleVisibility = (event) => {
+    event.preventDefault()
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>{blog.title}<button onClick={handleVisibility}>{visible?'hide':'view'}</button></div>
+      <div style={showWhenVisible}><a href={blog.url}>{blog.url}</a></div>
+      <div style={showWhenVisible}>likes {blog.likes} <button>like</button></div>
+      <div style={showWhenVisible}>{blog.author}</div>
+    </div>
+  )}
 
 export default Blog
