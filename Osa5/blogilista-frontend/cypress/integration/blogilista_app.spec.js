9f823a0a6e0d9ca16c2f6e6e68b2df65b3379032
Markku Kolkka <markku.kolkka@iki.fi>
2021-01-13 14:12:39 +0200

    Tehtävä 5.18

--- Osa5/blogilista-frontend/cypress/integration/blogilista_app.spec.js
+++ Osa5/blogilista-frontend/cypress/integration/blogilista_app.spec.js
@@ -1,10 +1,38 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Jane Blogger',
+      username: 'jblogger',
+      password: 'password'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
   it('shows login form', function() {
     cy.contains('log in to application')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('jblogger')
+      cy.get('#password').type('password')
+      cy.get('#login-button').click()
+
+      cy.contains('Jane Blogger logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('jblogger')
+      cy.get('#password').type('secret')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.get('html').should('not.contain', 'Jane Blogger logged in')
+    })
   })
 })
