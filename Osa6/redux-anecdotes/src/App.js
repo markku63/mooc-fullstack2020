5fcc38588b2d2f90e51d9f686a9a5e77b8dfdc7c
Markku Kolkka <markku.kolkka@iki.fi>
2021-01-21 09:52:47 +0200

    Tehtävä 6.10

--- Osa6/redux-anecdotes/src/App.js
+++ Osa6/redux-anecdotes/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Notification from './components/Notification'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   return (
     <div>
+      <Notification />
       <h2>Anecdotes</h2>
       <AnecdoteList />
       <AnecdoteForm />
