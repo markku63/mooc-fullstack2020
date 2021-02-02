5835dc964d6a96b1e854ed8bd44cb5ad70e0a2f9
Markku Kolkka <markku.kolkka@iki.fi>
2021-02-02 09:14:54 +0200

    Tehtävä 7.5

--- Osa7/routed-anecdotes/src/App.js
+++ Osa7/routed-anecdotes/src/App.js
@@ -67,10 +67,17 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onReset={handleReset}>
         <div>
           content
           <input name='content' {...content} />
@@ -83,7 +90,8 @@ const CreateNew = (props) => {
           url for more info
           <input name='info' {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='reset'>reset</button>
       </form>
     </div>
   )
