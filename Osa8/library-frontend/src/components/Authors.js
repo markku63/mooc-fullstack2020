6be1e17022d2c4f0c9f38f0d7ea34246c8e2ca28
Markku Kolkka <markku.kolkka@iki.fi>
2021-02-09 10:43:17 +0200

    Tehtävä 8.12

--- Osa8/library-frontend/src/components/Authors.js
+++ Osa8/library-frontend/src/components/Authors.js
@@ -1,11 +1,25 @@
   
-import React from 'react'
+import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { EDIT_AUTHOR } from '../queries'
 
 const Authors = (props) => {
+  const [name, setName] = useState('')
+  const [year, setYear] = useState(0)
+  const [ editAuthor ] = useMutation(EDIT_AUTHOR)
+
   if (!props.show) {
     return null
   }
   const authors = props.authors
+  
+  const submit = async (event) => {
+    event.preventDefault()
+
+    editAuthor({ variables: { name, setBornTo: year}})
+    setName('')
+    setYear(0)
+  }
 
   return (
     <div>
@@ -30,7 +44,27 @@ const Authors = (props) => {
           )}
         </tbody>
       </table>
-
+      <h2>Set year of birth</h2>
+      <form onSubmit={submit}>
+        <div>
+          <label htmlFor="name">name</label>
+          <select id="name" value={name} onChange={({target}) => setName(target.value)}>
+            {authors.map(a =>
+              <option key={a.id} value={a.name}>{a.name}</option>
+            )}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="born">born</label>
+          <input
+            type="number"
+            id="born"
+            value={year}
+            onChange={({target}) => setYear(Number(target.value))}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   )
 }
