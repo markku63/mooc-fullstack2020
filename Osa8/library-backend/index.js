e87ce1c5a60509ba587e67ef53f6750f85f4379e
Markku Kolkka <markku.kolkka@iki.fi>
2021-02-10 10:04:08 +0200

    Tehtävä 8.14

--- Osa8/library-backend/index.js
+++ Osa8/library-backend/index.js
@@ -23,90 +23,6 @@ mongoose.connect(
   console.log('error connecting to MongoDB:', error.message)
 })
 
-/*
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-*/
-/*
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
-*/
-/*
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
-*/
 const typeDefs = gql`
   type Author {
     name: String!
@@ -149,16 +65,18 @@ const resolvers = {
     bookCount: () => Book.collection.countDocuments(),
     authorCount: () => Author.collection.countDocuments(),
     allBooks: (root, args) => {
-      return Book.find({}).populate('author', { name: 1, born: 1 })
+      if (!args.author && !args.genre) {
+        return Book.find({}).populate('author')
+      } else if (!args.author) {
+        return Book.find({ genres: args.genre }).populate('author')
+      }
     },
     allAuthors: () => {
       return Author.find({})
     },
   },
   Author: {
-    bookCount: (root) => {
-      return 0 // books.filter(p => p.author === root.name).length
-    }
+    bookCount: (root) => Book.find({ author: root.id }).countDocuments()
   },
   Mutation: {
     addBook: async (root, args) => {
@@ -171,19 +89,18 @@ const resolvers = {
       }
       const book = new Book({ ...args, author: author.id })
       const savedBook = await book.save()
-      await savedBook.populate('author', { name: 1, born: 1 }).execPopulate()
+      await savedBook.populate('author').execPopulate()
       return savedBook
     },
-/*    editAuthor: (root, args) => {
-      const author = authors.find(p => p.name === args.name)
+    editAuthor: async (root, args) => {
+      const author = await Author.findOne({ name: args.name }).exec()
       if (!author) {
         return null
       }
-
-      const updatedAuthor = { ...author, born: args.setBornTo }
-      authors = authors.map(p => p.name === args.name ? updatedAuthor : p)
-      return updatedAuthor
-    }*/
+      author.born = args.setBornTo
+      await author.save()
+      return author
+    }
   }
 }
 
