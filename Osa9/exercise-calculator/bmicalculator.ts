83740c92b1c25f1cf62b8c9ace58c7e7844fccc6
Markku Kolkka <markku.kolkka@iki.fi>
2021-02-12 15:12:27 +0200

    Tehtävät 9.2 ja 9.3

--- Osa9/exercise-calculator/bmicalculator.ts
+++ Osa9/exercise-calculator/bmicalculator.ts
@@ -1,4 +1,12 @@
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
 const calculateBmi = (height: number, weight: number): string => {
+  if (height <= 0 || weight <= 0) {
+    throw new Error('Impossible inpu values')
+  }
   const bmi = weight / ((height/100)**2);
   if (bmi < 15) {
     return 'Very severely underweight';
@@ -19,4 +27,23 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 }
 
-console.log(calculateBmi(180, 74))
+const parseBmiArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+try {
+  const { height, weight } = parseBmiArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch(e) {
+  console.log('Error:', e.message);
+}
---
+++ Osa9/exercise-calculator/exerciseCalculator.ts
@@ -0,0 +1,65 @@
+interface TrainingResult {
+  periodLength: number;
+  trainingDays: number;
+  target: number;
+  average: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+}
+
+interface ExerciseValues {
+  dailyExercises: Array<number>;
+  target: number
+}
+
+const calculateExercises = (dailyExercises: Array<number>, target: number): TrainingResult => {
+  const periodLength = dailyExercises.length;
+  if (periodLength === 0) throw new Error('Empty exercise table');
+  const trainingDays = dailyExercises.reduce((total, curr) => { return (curr > 0 ? total + 1 : total)}, 0);
+  const average = dailyExercises.reduce((total, curr) => total + curr, 0) / periodLength;
+  const success = average >= target;
+  let rating: number;
+  let ratingDescription: string;
+  if (success) {
+    rating = 3;
+    ratingDescription = 'Excellent! Continue training at this level'
+  } else if (average >= 0.75 * target) {
+    rating = 2;
+    ratingDescription = 'not too bad but could be better'
+  } else {
+    rating = 1;
+    ratingDescription = 'you really should work harder'
+  }
+  return {periodLength, trainingDays, target, average, success, rating, ratingDescription};
+}
+
+const parseTrainingArguments = (args: Array<string>): ExerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  if (!isNaN(Number(args[2]))) {
+    const target = Number(args[2]);
+    let dailyExercises: Array<number> = [];
+    for (const s of args.slice(3)) {
+      const n = Number(s);
+      if (isNaN(n)) {
+        throw new Error('Provided values were not numbers!');
+      } else {
+        dailyExercises.push(n);
+      }
+    }
+    return {
+      dailyExercises,
+      target
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+try {
+  const {dailyExercises, target} = parseTrainingArguments(process.argv);
+  console.log(calculateExercises(dailyExercises, target));
+} catch (e) {
+  console.log('Error:', e.message);
+}
