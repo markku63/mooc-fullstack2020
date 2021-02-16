eb9227fa5c116ae987db7107e0875d26b4f523ef
Markku Kolkka <markku.kolkka@iki.fi>
2021-02-16 16:03:30 +0200

    Tehtävä 9.10

--- Osa9/patientor-backend/src/index.ts
+++ Osa9/patientor-backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagnosesRouter from './routes/diagnoses';
 import cors from 'cors';
 
 const app = express();
@@ -10,6 +11,8 @@ const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
