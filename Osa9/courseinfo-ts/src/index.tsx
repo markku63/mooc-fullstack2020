15cef447eac30e3240be22242ac2bc7465ceda3e
Markku Kolkka <markku.kolkka@iki.fi>
2021-02-18 12:00:32 +0200

    Tehtävä 9.15

--- Osa9/courseinfo-ts/src/index.tsx
+++ Osa9/courseinfo-ts/src/index.tsx
@@ -1,18 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-interface CoursePart {
+interface CoursePartBase {
   name: string,
   exerciseCount: number
 }
+
+interface CoursePartWithDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartOne extends CoursePartWithDescription {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartWithDescription {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+interface CoursePartFour extends CoursePartWithDescription {
+  name: "State handling";
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CoursePartFour;
+
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`);
+};
 
 const Header: React.FC<{ name: string }> = ({ name }) => (
   <h1>{name}</h1>
 );
 
+const Part: React.FC<{ part: CoursePart }> = ({ part }) => {
+  switch (part.name) {
+    case "Fundamentals":
+      return <p>{part.name} {part.exerciseCount} {part.description}</p>;
+    case "Using props to pass data":
+      return <p>{part.name} {part.exerciseCount} {part.groupProjectCount}</p>;
+    case "Deeper type usage":
+      return <p>{part.name} {part.exerciseCount} {part.description} <a href={part.exerciseSubmissionLink}>{part.exerciseSubmissionLink}</a></p>;
+    case "State handling":
+      return <p>{part.name} {part.exerciseCount} {part.description}</p>
+    default:
+      return assertNever(part);
+  }
+}
+
 const Content: React.FC<{ parts: CoursePart[] }> = ({ parts }) => (
   <div>
-    {parts.map((part, idx) => <p key={idx}>{part.name} {part.exerciseCount}</p>)}
+    {parts.map((part, idx) => <Part key={idx} part={part} />)}
   </div>
 );
 
@@ -25,18 +68,27 @@ const Total: React.FC<{ parts: CoursePart[] }> = ({ parts }) => (
 
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+    },
+    {
+      name: "State handling",
+      exerciseCount: 13,
+      description: "Handle your state like a pro"
     }
   ];
 
