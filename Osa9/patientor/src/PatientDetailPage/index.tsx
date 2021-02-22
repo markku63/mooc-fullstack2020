94e080aa601b2b6427b51ba8e245c8fafc8388fe
Markku Kolkka <markku.kolkka@iki.fi>
2021-02-22 09:16:58 +0200

    Tehtävä 9.17

--- Osa9/patientor/src/PatientDetailPage/index.tsx
+++ Osa9/patientor/src/PatientDetailPage/index.tsx
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { Container, Header, Icon } from 'semantic-ui-react';
-import { useStateValue } from "../state";
+import { useStateValue } from '../state';
+import { Patient } from '../types';
+import { apiBaseUrl } from '../constants';
 
 const PatientDetailPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const [{ patients }, dispatch] = useStateValue();
-  const [patient, setPatient] = useState(Object.values(patients).find(p => p.id === id));
+  const [patient, setPatient] = useState<Patient|undefined>(Object.values(patients).find(p => p.id === id));
   let genderIcon: 'man'|'woman'|'other gender';
+
+  useEffect(() => {
+    const fetchPatientDetail = async () => {
+      try {
+        const { data: patientFromApi } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+        setPatient(patientFromApi);
+        dispatch({ type: 'UPDATE_PATIENT', payload: patientFromApi});
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    // SSN isn't in the public patient data, only in individual details
+    if (!patient || !patient.ssn) {
+      fetchPatientDetail();
+    }
+  }, [id, patient]);
 
   if (!patient) {
     return null;
@@ -33,6 +52,7 @@ const PatientDetailPage: React.FC = () => {
       </Header>
       <div>ssn: {patient.ssn}</div>
       <div>occupation: {patient.occupation}</div>
+      <div>date of birth: {patient.dateOfBirth}</div>
     </Container>
   )
 };
